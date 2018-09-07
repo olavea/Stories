9cfcf6b7b62f595c28068b3d648fb58d93238a0b
Ola Vea <ola@olavea.com>
2018-09-07 10:15:04 +0200

    Create checkout2.js

---
+++ src/components/checkout2.js
@@ -0,0 +1,110 @@
+import React from 'react'
+
+// hardcoded amount (in US cents) to charge users
+// you could set this variable dynamically to charge different amounts
+const amount = 777
+const cardStyles = {
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-around',
+  alignItems: 'flex-start',
+  padding: '3rem',
+  boxShadow: '5px 5px 25px 0 rgba(46,61,73,.2)',
+  backgroundColor: '#fff',
+  borderRadius: '6px',
+  maxWidth: '400px',
+}
+const buttonStyles = {
+  fontSize: '13px',
+  textAlign: 'center',
+  color: '#fff',
+  outline: 'none',
+  padding: '12px 60px',
+  boxShadow: '2px 5px 10px rgba(0,0,0,.1)',
+  backgroundColor: 'rgb(255, 178, 56)',
+  borderRadius: '6px',
+  letterSpacing: '1.5px',
+}
+
+// Below is where the checkout component is defined.
+// It has several functions, and some default state variables.
+const Checkout2 = class extends React.Component {
+  state = {
+    disabled: false,
+    buttonText: 'BUY NOW',
+    paymentMessage: '',
+  }
+
+  resetButton() {
+    this.setState({ disabled: false, buttonText: 'BUY NOW' })
+  }
+
+  componentDidMount() {
+    this.stripeHandler = window.StripeCheckout.configure({
+      // You’ll need to add your own Stripe public key to the `checkout.js` file.
+      // key: 'pk_test_STRIPE_PUBLISHABLE_KEY',
+      key: 'pk_test_gILsad3S49ArZfiigy6RXlHu',
+      closed: () => {
+        this.resetButton()
+      },
+    })
+  }
+
+  openStripeCheckout(event) {
+    event.preventDefault()
+    this.setState({ disabled: true, buttonText: 'WAITING...' })
+    this.stripeHandler.open({
+      name: 'Test bøker',
+      amount: amount,
+      // Changes here amaount: index amount
+      shippingAddress: true,
+      billingAddress: false,
+      description: 'Denne testen er gratis, men boka er bra',
+      token: token => {
+        fetch(`AWS_LAMBDA_URL`, {
+          method: 'POST',
+          body: JSON.stringify({
+            token,
+            amount,
+          }),
+          headers: new Headers({
+            'Content-Type': 'application/json',
+          }),
+        })
+          .then(res => {
+            console.log('Transaction processed successfully')
+            this.resetButton()
+            this.setState({ paymentMessage: 'Payment Successful!' })
+            return res.json()
+          })
+          .catch(error => {
+            console.error('Error:', error)
+            this.setState({ paymentMessage: 'Payment Failed' })
+          })
+      },
+    })
+  }
+
+  render() {
+    return (
+      <div style={cardStyles}>
+        <h4>Kjøp min bok + e-bok + bullet journal!</h4>
+        <p>
+          Oppskrift: skriv din e-post, navn og adresse. Bruk 4242 4242 4242 4242
+          som kort skriv 3 tall som ... kode og en fremtidig dato som
+          utløpsdato.
+        </p>
+        <button
+          style={buttonStyles}
+          onClick={event => this.openStripeCheckout(event)}
+          disabled={this.state.disabled}
+        >
+          {this.state.buttonText}
+        </button>
+        {this.state.paymentMessage}
+      </div>
+    )
+  }
+}
+
+export default Checkout2
